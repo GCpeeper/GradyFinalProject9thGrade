9a4378a03343bec9ef8ee2e31684e438034ff4c2
GCpeeper <gcpeeper@hotmail.com>
2023-05-08 10:55:50 -0700

    Fixed Movement Issues with Player

--- Final Project/player.gd
+++ Final Project/player.gd
@@ -1,140 +1,162 @@
 extends CharacterBody2D
 
-enum State {IDLE, MOVE, ATTACK}
+enum State {IDLE, MOVE_RIGHT, MOVE_LEFT, MOVE_UP, MOVE_DOWN, ATTACK, DYING, DEAD}
+const MOVE_STATES = [State.MOVE_DOWN, State.MOVE_LEFT, State.MOVE_RIGHT, State.MOVE_UP]
+const MOVE_VECTORS = {
+	State.MOVE_LEFT: Vector2(-250,0),
+	State.MOVE_RIGHT: Vector2(250,0),
+	State.MOVE_UP: Vector2(0,-250),
+	State.MOVE_DOWN: Vector2(0,250)
+}
 
 var curstate = State.IDLE
-var speed = 5
-var lastmovedir: Vector2 = Vector2.ZERO
+var lastMoveState: State = State.MOVE_DOWN
 var lastdir: Vector2 = Vector2.ZERO
 var state_time = 0.0
+var health = 100
+var inside = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite2D.play("stand_down")
+	print("Player was made")
 	
 
 func switch_to(new_state: State):
 	curstate = new_state
 	state_time = 0.0
 	
-	if new_state == State.IDLE:	
-		$SwordArea.monitoring = false
-		
-		if lastmovedir.x > 0:
-			$AnimatedSprite2D.play("stand_right")
+	if new_state in MOVE_STATES:
+		if new_state == State.MOVE_RIGHT:
+			$AnimatedSprite2D.play("walk_right")
 			$AnimatedSprite2D.flip_h = false
-		elif lastmovedir.x < 0:
-			$AnimatedSprite2D.play("stand_right")
+			lastMoveState = State.MOVE_RIGHT
+		elif new_state == State.MOVE_LEFT:
+			$AnimatedSprite2D.play("walk_right")
 			$AnimatedSprite2D.flip_h = true
-		elif lastmovedir.y > 0:
+			lastMoveState = State.MOVE_LEFT
+		elif new_state == State.MOVE_UP:
+			$AnimatedSprite2D.play("walk_up")
+			$AnimatedSprite2D.flip_h = false
+			lastMoveState = State.MOVE_UP
+		elif new_state == State.MOVE_DOWN:
+			$AnimatedSprite2D.play("walk_down")
+			$AnimatedSprite2D.flip_h = false
+			lastMoveState = State.MOVE_DOWN
+	elif new_state == State.IDLE:
+		if lastMoveState == State.MOVE_DOWN:
 			$AnimatedSprite2D.play("stand_down")
 			$AnimatedSprite2D.flip_h = false
-		elif lastmovedir.y < 0:
+		elif lastMoveState == State.MOVE_UP:
 			$AnimatedSprite2D.play("stand_up")
-			$AnimatedSprite2D.flip_h = false 
-	elif new_state == State.ATTACK:
-		$AnimatedSprite2D.frame = 0
-		$SwordArea.monitoring = true
-		
-		if lastmovedir.x > 0:
-			$AnimatedSprite2D.play("swing_right")
 			$AnimatedSprite2D.flip_h = false
-		elif lastmovedir.x < 0:
-			$AnimatedSprite2D.play("swing_right")
+		elif lastMoveState == State.MOVE_RIGHT:
+			$AnimatedSprite2D.play("stand_right")
+			$AnimatedSprite2D.flip_h = false
+		elif lastMoveState == State.MOVE_LEFT:
+			$AnimatedSprite2D.play("stand_right")
 			$AnimatedSprite2D.flip_h = true
-		elif lastmovedir.y > 0:
+	elif new_state == State.ATTACK:
+		if lastMoveState == State.MOVE_DOWN:
 			$AnimatedSprite2D.play("swing_down")
 			$AnimatedSprite2D.flip_h = false
-		elif lastmovedir.y < 0:
+		elif lastMoveState == State.MOVE_UP:
 			$AnimatedSprite2D.play("swing_up")
 			$AnimatedSprite2D.flip_h = false
-	elif new_state == State.MOVE:
-		$SwordArea.monitoring = false
-		update_movement_animation()
+		elif lastMoveState == State.MOVE_RIGHT:
+			$AnimatedSprite2D.play("swing_right")
+			$AnimatedSprite2D.flip_h = false
+		elif lastMoveState == State.MOVE_LEFT:
+			$AnimatedSprite2D.play("swing_right")
+			$AnimatedSprite2D.flip_h = true
+	elif new_state == State.DYING:
+		if lastMoveState == State.MOVE_RIGHT:
+			$AnimatedSprite2D.play("dying")
+			$AnimatedSprite2D.flip_h = false
+		elif lastMoveState == State.MOVE_LEFT:
+			$AnimatedSprite2D.play("dying")
+			$AnimatedSprite2D.flip_h = true
+		if lastMoveState == State.MOVE_UP:
+			$AnimatedSprite2D.play("dying")
+			$AnimatedSprite2D.flip_h = false
+		elif lastMoveState == State.MOVE_DOWN:
+			$AnimatedSprite2D.play("dying")
+			$AnimatedSprite2D.flip_h = true
 
 		
 func update_movement_animation():
-	if curstate == State.MOVE:
-		if lastmovedir.x > 0:
+	if curstate in MOVE_STATES:
+		if curstate == State.MOVE_RIGHT:
 			$AnimatedSprite2D.play("walk_right")
 			$AnimatedSprite2D.flip_h = false
-		elif lastmovedir.x < 0:
+			lastMoveState = State.MOVE_RIGHT
+		elif curstate == State.MOVE_LEFT:
 			$AnimatedSprite2D.play("walk_right")
 			$AnimatedSprite2D.flip_h = true
-		elif lastmovedir.y > 0:
-			$AnimatedSprite2D.play("walk_down")
-			$AnimatedSprite2D.flip_h = false
-		elif lastmovedir.y < 0:
+			lastMoveState = State.MOVE_LEFT
+		elif curstate == State.MOVE_UP:
 			$AnimatedSprite2D.play("walk_up")
 			$AnimatedSprite2D.flip_h = false
+			lastMoveState = State.MOVE_UP
+		elif curstate == State.MOVE_DOWN:
+			$AnimatedSprite2D.play("walk_down")
+			$AnimatedSprite2D.flip_h = false
+			lastMoveState = State.MOVE_DOWN
+
 
 func _physics_process(delta):
-	var dir = Vector2.ZERO
-	
-	# Setup a movement vector based on keyboard input
-	if Input.is_action_pressed("moveup"):
-		dir.y = -1
-	elif Input.is_action_pressed("movedown"):
-		dir.y = 1	
-	elif Input.is_action_pressed("moveleft"):
-		dir.x = -1	
-	elif Input.is_action_pressed("moveright"):
-		dir.x = 1		
-		
-	# Apply that movement and save the last vectors as part of our state so we can select which
-	# animation to play layer
-	move_and_collide(dir * speed)	
-	lastdir = dir
-	
-	if dir.length() > 0:
-		lastmovedir = dir
-	
-	# Switch to different states, based on our current state, and the input received
-	if curstate == State.IDLE:
-		if Input.is_action_just_pressed("attack"):
-			switch_to(State.ATTACK)
-		elif dir.length() > 0:
-			switch_to(State.MOVE)
-	elif curstate == State.MOVE:
-		if Input.is_action_just_pressed("attack"):
-			switch_to(State.ATTACK)
-		elif dir.length() == 0:
-			switch_to(State.IDLE)
-	elif curstate == State.ATTACK:
-		if Input.is_action_just_pressed("attack"):
-			switch_to(State.ATTACK)
-		
-	# Update which animation is playing if you are in the movement state so it matches with the 
-	# player movement
-	update_movement_animation()
-	
-	# Keep track of how long we spent in the current state so far
 	state_time += delta
+	
+	if Input.is_action_pressed("moveup"):
+		switch_to(State.MOVE_UP)
+	elif Input.is_action_pressed("movedown"):
+		switch_to(State.MOVE_DOWN)
+	elif Input.is_action_pressed("moveleft"):
+		switch_to(State.MOVE_LEFT)
+	elif Input.is_action_pressed("moveright"):
+		switch_to(State.MOVE_RIGHT)
+	elif not Input.is_action_pressed("moveup"):
+		switch_to(State.IDLE)
+	elif not Input.is_action_pressed("movedown"):
+		switch_to(State.IDLE)
+	elif not Input.is_action_pressed("moveleft"):
+		switch_to(State.IDLE)
+	elif not Input.is_action_pressed("moveright"):
+		switch_to(State.IDLE)
+	elif Input.is_action_just_pressed("attack") and inside:
+		switch_to(State.ATTACK)
+	
+	
+	if curstate in MOVE_STATES:
+		var collided = move_and_collide(MOVE_VECTORS[curstate]*delta)
 
 
 func _on_sword_area_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	# Figure out which collision shape to use for our sword, and hit an enemy with it
+	inside = true
 	if curstate == State.ATTACK and body != self:
 		var struck = false
 		
-		if lastmovedir.x > 0 and local_shape_index == 0:
+		if lastMoveState == State.MOVE_RIGHT and local_shape_index == 0:
 			struck = true
-		elif lastmovedir.x < 0 and local_shape_index == 1:
+		elif lastMoveState == State.MOVE_LEFT and local_shape_index == 1:
 			struck = true
-		elif lastmovedir.y > 0 and local_shape_index == 2:
+		elif lastMoveState == State.MOVE_DOWN and local_shape_index == 2:
 			struck = true
-		elif lastmovedir.y < 0 and local_shape_index == 3:
+		elif lastMoveState == State.MOVE_UP and local_shape_index == 3:
 			struck = true
 		
 		if struck and body is Enemy:
 			body.hit()
 
 
-
 func _on_animated_sprite_2d_animation_finished():
-	# Change states if needed once animations finish
+		# Change states if needed once animations finish
 	if curstate == State.ATTACK:
-		if lastdir.length() > 0:
-			switch_to(State.MOVE)
-		else:
-			switch_to(State.IDLE)
+		switch_to(State.IDLE)
+	elif curstate == State.DYING:
+		switch_to(State.DEAD)
+
+
+func _on_sword_area_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
+	inside = false
